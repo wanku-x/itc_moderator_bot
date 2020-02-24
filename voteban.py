d7dddbb04633ae1ebb22c6344992a9a3cc2d7ffd
wanku-x <wanku@bk.ru>
2020-02-24 15:47:11 +0300

    Допилил кнопки для голосования

--- voteban.py
+++ voteban.py
@@ -235,19 +235,22 @@ def handle_callback_vote(bot, call):
         poll_id=poll.id,
     )
 
-    bot.edit_message_reply_markup(
-        chat_id=call.message.chat.id,
-        message_id=call.message.message_id,
-        reply_markup=create_poll_keyboard(
-            poll_results["votes_for_amount"],
-            poll_results["votes_against_amount"],
-        ),
-    )
-
-    bot.answer_callback_query(
-        callback_query_id=call.id,
-        text="Твой голос: {}".format(
-            "Да" if call.data == "vote_for" else "Нет"
-        ),
-        show_alert=False,
-    )
+    try:
+        bot.edit_message_reply_markup(
+            chat_id=call.message.chat.id,
+            message_id=call.message.message_id,
+            reply_markup=create_poll_keyboard(
+                poll_results["votes_for_amount"],
+                poll_results["votes_against_amount"],
+            ),
+        )
+    except:  # noqa
+        pass
+    finally:
+        bot.answer_callback_query(
+            callback_query_id=call.id,
+            text="Твой голос: {}".format(
+                "Да" if call.data == "vote_for" else "Нет"
+            ),
+            show_alert=False,
+        )
